d74559299cac4e2c9e1722bae869d19f71ae0f5b
DevExpressExampleBot <fakeEmail>
2019-08-05 17:12:03 +0300

    Source auto update [skip ci]

--- VB/RemoveItemsFromPopupMenu/Form1.vb
+++ VB/RemoveItemsFromPopupMenu/Form1.vb
@@ -5,25 +5,25 @@ Imports DevExpress.XtraPdfViewer.Extensions
 Imports DevExpress.XtraPdfViewer.Commands
 
 Namespace CustomPopupMenu
-    Partial Public Class Form1
-        Inherits Form
+	Partial Public Class Form1
+		Inherits Form
 
-        Public Sub New()
-            InitializeComponent()
-            pdfViewer1.LoadDocument("..\..\Demo.pdf")
-        End Sub
+		Public Sub New()
+			InitializeComponent()
+			pdfViewer1.LoadDocument("..\..\Demo.pdf")
+		End Sub
 
-        Private Sub pdfViewer1_PopupMenuShowing(ByVal sender As Object, ByVal e As PdfPopupMenuShowingEventArgs) Handles pdfViewer1.PopupMenuShowing
-            ' Hide the popup menu for the bookmark tree.
-            If e.PopupMenuKind = PdfPopupMenuKind.BookmarkTree Then
-                e.ItemLinks.Clear()
-            End If
+		Private Sub pdfViewer1_PopupMenuShowing(ByVal sender As Object, ByVal e As PdfPopupMenuShowingEventArgs) Handles pdfViewer1.PopupMenuShowing
+			' Hide the popup menu for the bookmark tree.
+			If e.PopupMenuKind = PdfPopupMenuKind.BookmarkTree Then
+				e.ItemLinks.Clear()
+			End If
 
-            ' Remove Rotate Clockwise and Rotate Counterclockwise items from the Page Content popup menu.
-            If e.PopupMenuKind = PdfPopupMenuKind.PageContent Then
-                e.ItemLinks.Remove(e.ItemLinks.GetPdfViewerBarItemLink(PdfViewerCommandId.RotatePageClockwise))
-                e.ItemLinks.Remove(e.ItemLinks.GetPdfViewerBarItemLink(PdfViewerCommandId.RotatePageCounterclockwise))
-            End If
-        End Sub
-    End Class
+			' Remove Rotate Clockwise and Rotate Counterclockwise items from the Page Content popup menu.
+			If e.PopupMenuKind = PdfPopupMenuKind.PageContent Then
+				e.ItemLinks.Remove(e.ItemLinks.GetPdfViewerBarItemLink(PdfViewerCommandId.RotatePageClockwise))
+				e.ItemLinks.Remove(e.ItemLinks.GetPdfViewerBarItemLink(PdfViewerCommandId.RotatePageCounterclockwise))
+			End If
+		End Sub
+	End Class
 End Namespace
