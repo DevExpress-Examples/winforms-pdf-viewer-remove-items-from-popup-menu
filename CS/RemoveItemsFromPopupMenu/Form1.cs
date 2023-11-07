85246c2bd72783bc03fbbbdf4d9fb5e6bbe7a1e0
guemjalee <polina.fedorova@devexpress.com>
2023-11-07 11:13:29 +0100

    add action

--- CS/RemoveItemsFromPopupMenu/Form1.cs
+++ CS/RemoveItemsFromPopupMenu/Form1.cs
@@ -6,7 +6,7 @@ using DevExpress.XtraPdfViewer.Commands;
 
 namespace CustomPopupMenu
 {
-    public partial class Form1 : Form
+    public partial class Form1 : DevExpress.XtraEditors.XtraForm
     {
         public Form1()
         {
@@ -16,6 +16,17 @@ namespace CustomPopupMenu
 
         void pdfViewer1_PopupMenuShowing(object sender, PdfPopupMenuShowingEventArgs e)
         {
+
+            // Create a bar button item.
+            BarButtonItem browseBarButton = new BarButtonItem();
+            browseBarButton.Caption = "Custom Item";
+
+            // Insert the bar buttom item into the PDF Viewer popup menu and start a new group.
+            e.ItemLinks.Add(browseBarButton, true);
+
+            // Handle the bar button click event.
+            browseBarButton.ItemClick += browseBarButton_ItemClick;
+
             // Hide the popup menu for the bookmark tree.
             if (e.PopupMenuKind == PdfPopupMenuKind.BookmarkTree)
             {
@@ -28,6 +39,11 @@ namespace CustomPopupMenu
                 e.ItemLinks.Remove(e.ItemLinks.GetPdfViewerBarItemLink(PdfViewerCommandId.RotatePageClockwise));
                 e.ItemLinks.Remove(e.ItemLinks.GetPdfViewerBarItemLink(PdfViewerCommandId.RotatePageCounterclockwise));
             }
+        }
+
+        void browseBarButton_ItemClick(object sender, ItemClickEventArgs e)
+        {
+            MessageBox.Show("ItemClick event fires");
         }
     }
 }
