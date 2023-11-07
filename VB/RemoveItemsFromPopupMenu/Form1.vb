411e93fec84208da53dba62221357250cf3bec18
DevExpressExampleBot <fakeEmail>
2023-11-07 14:22:46 +0400

    Source auto update [skip ci]

--- VB/RemoveItemsFromPopupMenu/Form1.vb
+++ VB/RemoveItemsFromPopupMenu/Form1.vb
@@ -1,12 +1,13 @@
 Imports System.Windows.Forms
 Imports DevExpress.XtraPdfViewer
+Imports DevExpress.XtraBars
 Imports DevExpress.XtraPdfViewer.Extensions
 Imports DevExpress.XtraPdfViewer.Commands
 
 Namespace CustomPopupMenu
 
     Public Partial Class Form1
-        Inherits Form
+        Inherits DevExpress.XtraEditors.XtraForm
 
         Public Sub New()
             InitializeComponent()
@@ -14,6 +15,13 @@ Namespace CustomPopupMenu
         End Sub
 
         Private Sub pdfViewer1_PopupMenuShowing(ByVal sender As Object, ByVal e As PdfPopupMenuShowingEventArgs)
+            ' Create a bar button item.
+            Dim browseBarButton As BarButtonItem = New BarButtonItem()
+            browseBarButton.Caption = "Custom Item"
+            ' Insert the bar buttom item into the PDF Viewer popup menu and start a new group.
+            e.ItemLinks.Add(browseBarButton, True)
+            ' Handle the bar button click event.
+            AddHandler browseBarButton.ItemClick, AddressOf browseBarButton_ItemClick
             ' Hide the popup menu for the bookmark tree.
             If e.PopupMenuKind = PdfPopupMenuKind.BookmarkTree Then
                 e.ItemLinks.Clear()
@@ -24,6 +32,10 @@ Namespace CustomPopupMenu
                 e.ItemLinks.Remove(e.ItemLinks.GetPdfViewerBarItemLink(PdfViewerCommandId.RotatePageClockwise))
                 e.ItemLinks.Remove(e.ItemLinks.GetPdfViewerBarItemLink(PdfViewerCommandId.RotatePageCounterclockwise))
             End If
+        End Sub
+
+        Private Sub browseBarButton_ItemClick(ByVal sender As Object, ByVal e As ItemClickEventArgs)
+            MessageBox.Show("ItemClick event fires")
         End Sub
     End Class
 End Namespace
